f856ab7402c01d3e7dae9a94b36d5d34018eb394
qgw13467 <51719533+qgw13467@users.noreply.github.com>
2021-06-05 00:47:20 +0900

    Update AttractionDAO.java

--- src/main/java/attraction/AttractionDAO.java
+++ src/main/java/attraction/AttractionDAO.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-import board.Board;
 
 
 
@@ -28,64 +27,82 @@ public class AttractionDAO {
 	}
 	
 	//모든 여행지 정보 가져오기
-	public ArrayList<Attraction> getList()
-	{
-		String SQL = "SELECT * FROM attraction";
-		
-		ArrayList<Attraction> list = new ArrayList<Attraction>();
-		
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			rs = pstmt.executeQuery();
+		public ArrayList<Attraction_> getList()
+		{
+			String SQL = "SELECT * FROM attraction";
 			
-			while(rs.next()) {
+			ArrayList<Attraction_> list = new ArrayList<Attraction_>();
+			
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				rs = pstmt.executeQuery();
+				
+				while(rs.next()) {
 
-				Attraction attraction = new Attraction();
-				attraction.setAttractionName(rs.getString(1));
-				attraction.setAttractionID(rs.getInt(2));
-				attraction.setAddr1(rs.getString(3));
-				attraction.setThema(rs.getInt(4));
-				attraction.setMapX(rs.getFloat(5));
-				attraction.setMapY(rs.getFloat(6));
-				attraction.setAttractionScore(rs.getFloat(7));
-				list.add(attraction);
+					Attraction_ attraction = new Attraction_();
+					attraction.setID(rs.getString(1));
+					attraction.setScore(rs.getFloat(2));
+					attraction.setTitle(rs.getString(3));
+					attraction.setAddr(rs.getString(4));
+					attraction.setX(rs.getString(5));
+					attraction.setY(rs.getString(6));
+					attraction.setFirstImg(rs.getString(7));
+					attraction.setFirstImg2(rs.getString(8));
+					attraction.setOverview(rs.getString(9));
+					attraction.setThemas1(rs.getFloat(10));
+					attraction.setThemas2(rs.getFloat(11));
+					attraction.setThemas3(rs.getFloat(12));
+					attraction.setThemas4(rs.getFloat(13));
+					attraction.setThemas5(rs.getFloat(14));
+					attraction.setThemas6(rs.getFloat(15));
+					attraction.setThemas7(rs.getFloat(16));
+					list.add(attraction);
 
+				}
+							
+			} catch(Exception e) {
+				e.printStackTrace();
 			}
-						
-		} catch(Exception e) {
-			e.printStackTrace();
+			return list;
 		}
-		return list;
-	}
-	
-	
-	//여행지 정보 받아오기
-	public Attraction getAttractionInfo(int attractionID) {
 		
-		String SQL = "SELECT * FROM attraction WHERE attractionID = ?";
 		
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			pstmt.setInt(1,  attractionID);
-			rs= pstmt.executeQuery();
-			if(rs.next())
-			{
-				Attraction attraction = new Attraction();
-				attraction.setAttractionName(rs.getString(1));
-				attraction.setAttractionID(rs.getInt(2));
-				attraction.setAddr1(rs.getString(3));
-				attraction.setThema(rs.getInt(4));
-				attraction.setMapX(rs.getFloat(5));
-				attraction.setMapY(rs.getFloat(6));
-				attraction.setAttractionScore(rs.getFloat(7));
-				return attraction;
+		//여행지 정보 받아오기
+		public Attraction_ getAttractionInfo(int attractionID) {
+			
+			String SQL = "SELECT * FROM attraction WHERE attractionID = ?";
+			
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				pstmt.setInt(1,  attractionID);
+				rs= pstmt.executeQuery();
+				if(rs.next())
+				{
+					Attraction_ attraction = new Attraction_();
+					attraction.setID(rs.getString(1));
+					attraction.setScore(rs.getFloat(2));
+					attraction.setTitle(rs.getString(3));
+					attraction.setAddr(rs.getString(4));
+					attraction.setX(rs.getString(5));
+					attraction.setY(rs.getString(6));
+					attraction.setFirstImg(rs.getString(7));
+					attraction.setFirstImg2(rs.getString(8));
+					attraction.setOverview(rs.getString(9));
+					attraction.setThemas1(rs.getFloat(10));
+					attraction.setThemas2(rs.getFloat(11));
+					attraction.setThemas3(rs.getFloat(12));
+					attraction.setThemas4(rs.getFloat(13));
+					attraction.setThemas5(rs.getFloat(14));
+					attraction.setThemas6(rs.getFloat(15));
+					attraction.setThemas7(rs.getFloat(16));
+					return attraction;
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
 			}
-		} catch(Exception e) {
-			e.printStackTrace();
+			return null; 
+			
+			
 		}
-		return null; 
 		
-		
-	}
-	
 }
