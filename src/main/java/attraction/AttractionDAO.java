e28623faebdb338c17680287fa9192c74b9d9b30
qgw13467 <51719533+qgw13467@users.noreply.github.com>
2021-11-09 04:27:52 +0900

    review

--- src/main/java/attraction/AttractionDAO.java
+++ src/main/java/attraction/AttractionDAO.java
@@ -157,6 +157,21 @@ public class AttractionDAO {
 			
 			
 		}
+		
+		//평점 갱신
+		public int updateScore(String attractionID,float score) {
+			String SQL = "UPDATE attraction SET score =? WHERE id = ?";
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				pstmt.setFloat(1,  score);
+				pstmt.setString(2,  attractionID);
+				return pstmt.executeUpdate();
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			return -1; 
+		}
 
 		
 
